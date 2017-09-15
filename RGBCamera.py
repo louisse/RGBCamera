770c465310f9626e870e12db08e9514875d565e3
Louisse <louisseeustaquio@gmail.com>
2017-09-15 22:02:51 +0800

    ready code for socket

--- RGBCamera.py
+++ RGBCamera.py
@@ -1,21 +1,21 @@
 import cv2
 import numpy as np
 
-#face_cascade = cv2.CascadeClassifier('data/cascadeH5.xml')
-#face_cascade = cv2.CascadeClassifier('data/cascadG.xml')
-face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
-#face_cascade = cv2.CascadeClassifier('data/haarcascade_fullbody.xml')
+head_cascade = cv2.CascadeClassifier('data/cascadeH5.xml')
+body_cascade = cv2.CascadeClassifier('data/cascadG.xml')
+#face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+#fbody_cascade = cv2.CascadeClassifier('data/haarcascade_fullbody.xml')
 cap = cv2.VideoCapture(0)
 while True:
   _, frame = cap.read()
-  #frame = cv2.imread('body.jpg')
   gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
   
-  faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+  heads = head_cascade.detectMultiScale(gray, 1.05, 5)
   for (x,y,w,h) in faces:
     cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
-  cv2.imshow("frame",frame)
-  #cv2.imshow("gray",gray)
+  
+  #insert code to send to socket
+  
   k = cv2.waitKey(10) & 0xFF
   if k == 27:
     break
