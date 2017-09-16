61a86cb2b0976aec78f0eae55c41a53b90bb59de
Louisse <louisseeustaquio@gmail.com>
2017-09-16 21:04:26 +0800

    Adjusted width and height. removed waitKey function

--- RGBCamera.py
+++ RGBCamera.py
@@ -6,8 +6,8 @@ body_cascade = cv2.CascadeClassifier('data/cascadG.xml')
 #face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 #fbody_cascade = cv2.CascadeClassifier('data/haarcascade_fullbody.xml')
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 80)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 60)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
 
 while True:
   _, frame = cap.read()
@@ -28,8 +28,4 @@ while True:
     detected = heads.shape[0]
 
   #insert code to send to socket
-
-  k = cv2.waitKey(10) & 0xFF
-  if k == 27:
-    break
 cap.release()
